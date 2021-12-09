ed33366f1efd566e86f8247adb5313ab702973de
konmenel <konmenel@gmail.com>
2021-12-09 21:37:18 +0000

    Added template

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+// typedef input_t
+
+void parse_inputs()
+{
+
+    return;
+}
+
+
+int part_1()
+{
+
+    return -1;
+}
+
+
+int main() 
+{
+
+    return 0;
+}
