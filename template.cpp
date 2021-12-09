f576f6db017a677fa5f566371429e35da436b687
konmenel <konmenel@gmail.com>
2021-12-09 22:46:44 +0000

    More includes, typedefs etc

--- template.cpp
+++ template.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+
 
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
 
-// typedef input_t
 
-void parse_inputs()
+typedef void input_t;
+
+
+input_t parse_inputs()
 {
 
     return;
@@ -24,6 +29,11 @@ int part_1()
 
 int main() 
 {
+
+    // cout << "---PART 1---\n";
+    // cout << "*TEXT 1 HERE*" << var_name_1 << endl;
+    // cout << "---PART 2---\n";
+    // cout << "*TEXT 2 HERE*" << var_name_2 << endl;
 
     return 0;
 }
